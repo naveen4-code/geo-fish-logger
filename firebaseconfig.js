3b4cae05dbe289f51047c1237dea93da06b50751
Naveen Raj K <knaveenraj725@gmail.com>
2025-07-17 07:47:41 +0530

    Update firebaseconfig.js

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDCv_ilNgMS6GzjZiEoPAv4HOiFZJZ2bwk",
+  apiKey: "your_api_key",
   authDomain: "geo-tagged-fish-catcher.firebaseapp.com",
   projectId: "geo-tagged-fish-catcher",
   storageBucket: "geo-tagged-fish-catcher.appspot.com",
-  messagingSenderId: "546391140370",
-  appId: "1:546391140370:web:3c7bd00bc9c215d66450cb"
+  messagingSenderId: "your_messenger_id",
+  appId: "your_app_id"
 };
 firebase.initializeApp(firebaseConfig);
